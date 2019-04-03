1c94ce03577dfecf906b7fe3355dc1edc2794821
Leeladurga <leeladurgaprasad@aol.com>
2019-04-03 20:49:02 +0530

    first commit

---
+++ src/main/java/com/ps/sg/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,18 @@
+package com.ps.sg.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+
+@EnableHystrixDashboard
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}
